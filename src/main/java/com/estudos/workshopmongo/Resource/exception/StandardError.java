f4038e89ec2d02f9efab562bc65ae1f34855d55f
LucazsLO <lucaslure2.0@gmail.com>
2026-07-17 15:00:09 -0300

    retrieving user by ID

---
+++ src/main/java/com/estudos/workshopmongo/Resource/exception/StandardError.java
@@ -0,0 +1,60 @@
+package com.estudos.workshopmongo.Resource.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+    private String message;
+    public StandardError(Long timestamp, Integer status, String error, String path, String message) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.path = path;
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
--- src/main/java/com/estudos/workshopmongo/Services/UserService.java
+++ src/main/java/com/estudos/workshopmongo/Services/UserService.java
@@ -1,12 +1,14 @@
 package com.estudos.workshopmongo.Services;
 
 import com.estudos.workshopmongo.Repository.UserRepository;
+import com.estudos.workshopmongo.Services.exception.ObjectNotFoundException;
 import com.estudos.workshopmongo.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,12 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        if (user.isEmpty()) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user.get();
     }
 }
