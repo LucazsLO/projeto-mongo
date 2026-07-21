7dcf9756ad95aa62de15f9c83c0898a2d857c300
LucazsLO <lucaslure2.0@gmail.com>
2026-07-21 13:06:34 -0300

    add comments to post entity

--- src/main/java/com/estudos/workshopmongo/domain/Post.java
+++ src/main/java/com/estudos/workshopmongo/domain/Post.java
@@ -1,10 +1,14 @@
 package com.estudos.workshopmongo.domain;
 
 import com.estudos.workshopmongo.DTO.AuthorDTO;
+import com.estudos.workshopmongo.DTO.CommentDTO;
 import org.springframework.data.annotation.Id;
 
+import javax.xml.stream.events.Comment;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class Post implements Serializable {
@@ -16,6 +20,8 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
+
+    private List<CommentDTO> comments = new ArrayList<>();
     public Post(){
 
     }
@@ -67,6 +73,14 @@ public class Post implements Serializable {
     public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -78,6 +92,7 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 
 
 }
